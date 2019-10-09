dd60038bbd2cb7f5cad60dbd1124a781a2803cf8
Jules Bonard <julestim.bonard@gmail.com>
2019-10-09 09:42:06 +0200

    fini

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './App.css';
+import './components/Contact.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-
+      <Contact className="Contact" 
+      avatar="https://randomuser.me/api/portraits/women/79.jpg"
+      alt="image profil Priscilla Burke"
+      name="Priscilla Burke"
+      online
+      status="true"
+      />
+      <Contact className="Contact" 
+      avatar="https://randomuser.me/api/portraits/men/79.jpg"
+      alt="image profil Shane Warren"
+      name="Shane Warren"
+      />
+      <Contact className="Contact" 
+      avatar="https://randomuser.me/api/portraits/women/17.jpg"
+      alt="image profil Vicki Watkins"
+      name="Vicki Watkins"
+      online="true"
+      status="true"
+      />
     </div>
   );
 }
