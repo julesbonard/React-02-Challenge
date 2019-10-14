6ceeafb384da282bb3730b253a9c237d086c19ad
Jules Bonard <julestim.bonard@gmail.com>
2019-10-14 11:47:42 +0200

    fini React 05

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,20 +3,36 @@ import './Contact.css';
 import './QuoteCard.css';
 
 
-const Contact = ({avatar, alt, name,status,online}) => {
-    return(
-        <div className="Contact">
-            <img src={avatar} alt={alt} className="avatar"/>
-            <div>
-                <h1 className="name">{name}</h1>
-                <div className="status stats">
-                    <div className={online ? "status-online" : "status-offline"}></div>
-                    <div className="status-text">{status ? 'online': 'offline'}</div>
-                </div>
+class Contact extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            online: this.props.online
+        }
+    }
 
+    render(){
+        const {avatar, alt, name} = this.props;
+        const {online} = this.state;
+        
+        return(
+            <div className="Contact">
+                <img src={avatar} alt={alt} className="avatar"/>
+                <div>
+                    <h1 className="name">{name}</h1>
+                    <div className="status stats">
+                        <div className={online ? "status-online" : "status-offline"}
+                        onClick={() => {
+                            const newOnline = !online;
+                            this.setState({online : newOnline});
+                        }}
+                        ></div>
+                        <div className="status-text">{online ? 'online': 'offline'}</div>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Contact;
